7d67448d437e4708813e0da4844f77fd3d2d2661
Grace <gracerstuart@gmail.com>
2016-06-10 17:23:07 -0700

    Add folder structure.

---
+++ ex2/ex2.2-gs.rb
@@ -0,0 +1,3 @@
+# Yes, I found an error: The following included "us" when it should have been "use"
+# You can also us a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
