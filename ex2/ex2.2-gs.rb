9b889bd04fc72d3d9f4d5015492284075d682cde
Robert Nyholm <robnyholm@gmail.com>
2016-06-17 03:12:02 -0700

    Removing directories

--- ex2/ex2.2-gs.rb
+++
@@ -1,3 +0,0 @@
-# Yes, I found an error: The following included "us" when it should have been "use"
-# You can also us a comment to "disable" or comment out a piece of code:
-# puts "This won't run."
